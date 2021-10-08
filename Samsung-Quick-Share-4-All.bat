e98dd366f1e01a7027c983e9e2e03e0ce431680b
Giovanni Magliocchetti <62136803+obrobrio2000@users.noreply.github.com>
2021-10-09 01:56:35 +0200

    Add files via upload

---
+++ Samsung-Quick-Share-4-All.bat
@@ -0,0 +1,1 @@
+reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /d "NP950QDB-KA1US" /f
---
+++ Samsung-Quick-Share-4-All.ps1
@@ -0,0 +1,1 @@
+Set-Itemproperty -path 'HKLM:\HARDWARE\DESCRIPTION\System\BIOS' -Name 'SystemProductName' -value 'NP950QDB-KA1US'
