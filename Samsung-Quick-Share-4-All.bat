c30408c1ac1501020eef092a9a674436126d0dd2
Giovanni Magliocchetti <62136803+obrobrio2000@users.noreply.github.com>
2021-10-14 16:38:11 +0200

    Update Samsung-Quick-Share-4-All.bat

--- Samsung-Quick-Share-4-All.bat
+++ Samsung-Quick-Share-4-All.bat
@@ -1,1 +1,2 @@
 reg add "HKLM\HARDWARE\DESCRIPTION\System\BIOS" /v SystemProductName /t REG_SZ /d "NP950QDB-KA1US" /f
+powershell -command "Set-Itemproperty -path 'HKLM:\HARDWARE\DESCRIPTION\System\BIOS' -Name 'SystemProductName' -value 'NP950QDB-KA1US'"
